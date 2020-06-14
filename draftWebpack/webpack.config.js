4c6f1354ca2293b8fefc092fb3c7ca103488d5f5
Petro Fedorenko <petewaitforletters@gmail.com>
2020-06-14 17:57:18 +0300

    add clear plugin

--- draftWebpack/webpack.config.js
+++ draftWebpack/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Webpack',
     }),
