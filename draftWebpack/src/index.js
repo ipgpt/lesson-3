f05be7cbfb52132d9f2de01693f42f2904a3dbe9
Petro Fedorenko <petewaitforletters@gmail.com>
2020-06-14 17:30:47 +0300

    add image loader

--- draftWebpack/src/index.js
+++ draftWebpack/src/index.js
@@ -1,10 +1,16 @@
 import './style.css';
+import Banner from './banner.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = 'Hello Webpack!';
   element.classList.add('message');
+
+  const bannerImage = new Image();
+  bannerImage.src = Banner;
+
+  element.appendChild(bannerImage);
 
   return element;
 }
